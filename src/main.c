dbbec4573b291594e592bf1ac138727312e64d53
Zack Munich <zack.munich@gmail.com>
2017-01-11 15:58:53 -0500

    added reference upon bot shutdown

--- src/main.c
+++ src/main.c
@@ -249,5 +249,7 @@ int main(int argc, char *argv[])
     }
     initialize(botPass, &botMsg);
     run(&botMsg);
+    strcpy(botMsg.text,"/me whispers rosebud");
+    chat(&botMsg);
     return 0;
 }
